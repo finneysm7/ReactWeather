5aba38b799ca1df1bcc9a272d307bfbf8e8ea187
Sean Finney <mcfinneys@gmail.com>
2017-03-19 19:43:07 -0700

    Love to love

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -4,7 +4,7 @@ var About = (props) => {
   return (
     <div>
       <h3>About</h3>
-      <p>Welcome to the about page! This weather app created with Love by <a target="_blank" href="https://github.com/finneysm7">Sean Finney</a></p>
+      <p>Welcome to the about page! This weather app created with love by <a target="_blank" href="https://github.com/finneysm7">Sean Finney</a></p>
     </div>
   )
 };
