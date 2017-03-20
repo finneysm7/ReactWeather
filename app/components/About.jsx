c9a6a04ec9596ad9cad5dacdf8834d4bec3d8468
Sean Finney <mcfinneys@gmail.com>
2017-03-19 21:00:05 -0700

    Style about page, link to resources

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,8 +3,20 @@ var React = require('react');
 var About = (props) => {
   return (
     <div>
-      <h3>About</h3>
-      <p>Welcome to the about page! This weather app created with love by <a target="_blank" href="https://github.com/finneysm7">Sean Finney</a></p>
+      <h1 className="text-center">About</h1>
+      <p>web application built with react. I built this for The Complete React Web Developer Course.</p>
+      <p>Here is some info about this page:</p>
+      <ul>
+        <li>
+          This weather app uses the <a target="_blank" href="http://openweathermap.org/api">open weather map</a> api
+        </li>
+        <li>
+          The github repo for this site can be found <a target="_blank" href="https://github.com/finneysm7/ReactWeather">here</a>
+        </li>
+        <li>
+          This weather app created with love by <a target="_blank" href="http://seanmcgowanfinney.com">Sean Finney</a>
+        </li>
+      </ul>
     </div>
   )
 };
